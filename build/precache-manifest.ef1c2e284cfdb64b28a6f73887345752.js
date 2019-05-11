2f5b4e7b7dbff61bae828a3af1a1b6b896ad390f
sklccchenchen <2934401021@qq.com>
2019-05-11 16:46:17 +0800

    build

---
+++ build/precache-manifest.ef1c2e284cfdb64b28a6f73887345752.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0886ab8ce9265dbfca6e13ab365b283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3414b097ec5d5000be",
+    "url": "/static/js/2.f7844318.chunk.js"
+  },
+  {
+    "revision": "6c64fc353a96663af57a",
+    "url": "/static/js/main.767689df.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
